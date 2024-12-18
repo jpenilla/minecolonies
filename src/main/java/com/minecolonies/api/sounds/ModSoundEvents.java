2a176a38cda623223c7926d3cb96515b8d11df98
Raycoms <ray.neiheiser@gmail.com>
2024-12-18 10:49:12 +0100

    hotfix derp

--- src/main/java/com/minecolonies/api/sounds/ModSoundEvents.java
+++ src/main/java/com/minecolonies/api/sounds/ModSoundEvents.java
@@ -92,6 +92,7 @@ public final class ModSoundEvents
                 individualSounds.add(new Tuple<>(maleSoundEvent, femaleSoundEvent));
                 individualSounds.add(new Tuple<>(maleSoundEvent, femaleSoundEvent));
             }
+            map.put(event, individualSounds);
         }
         CITIZEN_SOUND_EVENTS.put("child", map);
 
